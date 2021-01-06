712709f1070d671d5fd7c95bb228741517ff1c63
khadijaezzayany <57987891+khadijaezzayany@users.noreply.github.com>
2021-01-06 21:20:53 +0100

    add levels

--- Quiz Game/src/Quiz.java
+++ Quiz Game/src/Quiz.java
@@ -1,99 +1,54 @@
+package desplayQuiz;
 
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 
-public class Quiz implements ActionListener{
-	
-	String[] questions ={
-			"JAVA est un langage",
-			"Toutes les classes hÈritent de la classe",
-			"Par convention une classe ",
-			"Est-ce que on peut avoir plusieurs constructeurs pour la mÍme classe ",
+public class Quiz implements ActionListener {
+
+	String[] questions = { "JAVA est un langage", "Toutes les classes h√©ritent de la classe",
+			"Par convention une classe ", "Est-ce que on peut avoir plusieurs constructeurs pour la m√™me classe ",
 			"Dans la ligne public class A implements B, B est:",
-			"Class Test{\r\n"
-					+ "Public Test () {\r\n"
-					+ "System.out.println(îBonjourî);}\r\n"
-					+ "public Test (int i) {\r\n"
-					+ "this();\r\n"
-					+ "System.out.println(îNous sommes en î+i+ î !î);};\r\n"
-					+ "}\r\n"
-					+ "quíaffichera líinstruction suivante?\r\n"
-					+ "Test t1=new Test (2018);\r\n"
-					+ "",
-			
-					"Voici un constructeur de la classe Employee, y-at'il un problËme ?\r\n"
-					+ "Public void Employe(String n){\r\n"
-					+ "Nom=n;}",
-			"Pour spÈcifier que la variable ne pourra plus Ítre modifiÈe et doit Ítre initialisÈe dËs sa dÈclaration, on la dÈclare comme une constante avec le mot rÈservÈ",
-			"Dans une classe, on accËde ‡ ses variables gr‚ce au mot clÈ",
-			"calculerSalaire(int)\r\n"
-					+ "calculerSalaire(int, double)\r\n"
-					+ "La mÈthode calculerSalaire est :",
-			"Une classe qui contient au moins une mÈthode abstraite doit Ítre dÈclarÈe abstraite.",
-			"Est-ce quíune classe peut implÈmenter plusieurs interfaces ?",
-			"La dÈclaration d'une mÈthode suivante :\r\n"
-					+ "public void traitement() throws IOException\r\n"
-					+ "prÈcise que la mÈthode propage une exception\r\n"
-					+ "contrÙlÈe\r\n"
-					+ "",
-			"class Test{\r\n"
-					+ "public static void main (String[] args) {\r\n"
-					+ "try {\r\n"
-					+ "int a =10;\r\n"
-					+ "System.out.println (\"a = \" + a );\r\n"
-					+ "int b = 0 / a;\r\n"
-					+ "System.out.println (\"b = \" + b);\r\n"
-					+ "}\r\n"
-					+ "catch(ArithmeticException e)\r\n"
-					+ "{System.out.println (\"diviser par 0!\"); }\r\n"
-					+ "finally\r\n"
-					+ "{System.out.println (\"je suis ‡ líintÈrieur de\r\n"
-					+ "finally\");}}}"
-							};
-	String[][] options = 	{
-			{"CompilÈ", "InterprÈtÈ","CompilÈ et interpretÈ"},
-			{"Main", "Object", "AWT"},
-			{"est en minuscule", "commence par une majuscule", "est en majuscule"},
-			{"oui", "non"},
-			{"interface", "Class parent", "Package"},
-			{"AprËs la compilation, un programme Ècrit en JAVA, il se transforme en programme en bytecode Quelle est líextension du programme en bytecode?"},
-			{"aucun des choix", ".JAVA", ".Class"},
-			{"aucun des choix", "Bonjour Nous sommes en 2018!", "Nous sommes en 2018!"},
-			{"vrai", "faux", "aucun des choix"},
-			{"aucun des choix", "final", "const"},
-			{"aucun des choix", "this", "super"},
-			{"aucun des choix", "surchargÈe", "redÈfinie"},
-			{"vrai", "faux", "aucun des choix"},
-			{"vrai", "faux", "aucun des choix"},
-			{"vrai", "faux", "aucun des choix"},
-			{"aucun des choix", "a=10 b=0 Je suis ‡ líintÈrieur de finally", "a=10 b=0 diviser par 0! je suis ‡ líintÈrieur de finally"}
-							};
-	char[] answers = 		{
-			'A',
-            'C',
-            'B',
-            'A',
-            'A',
-            'A',
-            'C',
-            'A',
-            'C',
-            'B',
-            'A',
-            'A',
-            'B',
-            'A',
-            'B'
-							};
+			"Class Test{\r\n" + "Public Test () {\r\n" + "System.out.println(‚ÄùBonjour‚Äù);}\r\n"
+					+ "public Test (int i) {\r\n" + "this();\r\n"
+					+ "System.out.println(‚ÄùNous sommes en ‚Äù+i+ ‚Äù !‚Äù);};\r\n" + "}\r\n"
+					+ "qu‚Äôaffichera l‚Äôinstruction suivante?\r\n" + "Test t1=new Test (2018);\r\n" + "",
+
+			"Voici un constructeur de la classe Employee, y-at'il un probl√®me ?\r\n"
+					+ "Public void Employe(String n){\r\n" + "Nom=n;}",
+			"Pour sp√©cifier que la variable ne pourra plus √™tre modifi√©e et doit √™tre initialis√©e d√®s sa d√©claration, on la d√©clare comme une constante avec le mot r√©serv√©",
+			"Dans une classe, on acc√®de √† ses variables gr√¢ce au mot cl√©",
+			"calculerSalaire(int)\r\n" + "calculerSalaire(int, double)\r\n" + "La m√©thode calculerSalaire est :",
+			"Une classe qui contient au moins une m√©thode abstraite doit √™tre d√©clar√©e abstraite.",
+			"Est-ce qu‚Äôune classe peut impl√©menter plusieurs interfaces ?",
+			"La d√©claration d'une m√©thode suivante :\r\n" + "public void traitement() throws IOException\r\n"
+					+ "pr√©cise que la m√©thode propage une exception\r\n" + "contr√¥l√©e\r\n" + "",
+			"class Test{\r\n" + "public static void main (String[] args) {\r\n" + "try {\r\n" + "int a =10;\r\n"
+					+ "System.out.println (\"a = \" + a );\r\n" + "int b = 0 / a;\r\n"
+					+ "System.out.println (\"b = \" + b);\r\n" + "}\r\n" + "catch(ArithmeticException e)\r\n"
+					+ "{System.out.println (\"diviser par 0!\"); }\r\n" + "finally\r\n"
+					+ "{System.out.println (\"je suis √† l‚Äôint√©rieur de\r\n" + "finally\");}}}" };
+	String[][] options = { { "Compil√©", "Interpr√©t√©", "Compil√© et interpret√©" }, { "Main", "Object", "AWT" },
+			{ "est en minuscule", "commence par une majuscule", "est en majuscule" }, { "oui", "non" },
+			{ "interface", "Class parent", "Package" },
+			{ "Apr√®s la compilation, un programme √©crit en JAVA, il se transforme en programme en bytecode Quelle est l‚Äôextension du programme en bytecode?" },
+			{ "aucun des choix", ".JAVA", ".Class" },
+			{ "aucun des choix", "Bonjour Nous sommes en 2018!", "Nous sommes en 2018!" },
+			{ "vrai", "faux", "aucun des choix" }, { "aucun des choix", "final", "const" },
+			{ "aucun des choix", "this", "super" }, { "aucun des choix", "surcharg√©e", "red√©finie" },
+			{ "vrai", "faux", "aucun des choix" }, { "vrai", "faux", "aucun des choix" },
+			{ "vrai", "faux", "aucun des choix" }, { "aucun des choix", "a=10 b=0 Je suis √† l‚Äôint√©rieur de finally",
+					"a=10 b=0 diviser par 0! je suis √† l‚Äôint√©rieur de finally" } };
+	char[] answers = { 'A', 'C', 'B', 'A', 'A', 'A', 'C', 'A', 'C', 'B', 'A', 'A', 'B', 'A', 'B' };
 	char guess;
 	char answer;
+	int nextqust;
 	int index;
-	int correct_guesses =0;
+	int correct_guesses = 0;
 	int total_questions = questions.length;
 	int result;
-	int seconds=10;
-	
+	int seconds = 10;
+
 	JFrame frame = new JFrame();
 	JTextField textfield = new JTextField();
 	JTextArea textarea = new JTextArea();
@@ -105,110 +60,119 @@ public class Quiz implements ActionListener{
 	JLabel answer_labelC = new JLabel();
 	JLabel time_label = new JLabel();
 	JLabel seconds_left = new JLabel();
+	JLabel label = new JLabel();
 	JTextField number_right = new JTextField();
 	JTextField percentage = new JTextField();
-	
+	JTextField niveau = new JTextField();
 	Timer timer = new Timer(1000, new ActionListener() {
-		
+
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			seconds--;
 			seconds_left.setText(String.valueOf(seconds));
-			if(seconds<=0) {
+			if (seconds <= 0) {
 				displayAnswer();
 			}
-			}
-		});
-	
+		}
+	});
+
 	public Quiz() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(650,650);
+		frame.setSize(650, 650);
 		frame.getContentPane().setBackground(new Color(0, 0, 0));
 		frame.setLayout(null);
 		frame.setResizable(false);
-		
-		textfield.setBounds(0,0,650,50);
+
+		textfield.setBounds(0, 0, 650, 50);
 		textfield.setBackground(new Color(255, 0, 0));
 		textfield.setForeground(new Color(255, 230, 51));
-		textfield.setFont(new Font("Ink Free",Font.BOLD,30));
+		textfield.setFont(new Font("Ink Free", Font.BOLD, 30));
 		textfield.setBorder(BorderFactory.createBevelBorder(1));
 		textfield.setHorizontalAlignment(JTextField.CENTER);
 		textfield.setEditable(false);
-		
-		textarea.setBounds(0,50,650,50);
+
+		// niveau
+		niveau.setBounds(255, 455, 130, 50);
+		niveau.setBackground(null);
+		niveau.setForeground(Color.green);
+		niveau.setFont(new Font("MV Boli", Font.BOLD, 50));
+		niveau.setEditable(false);
+		niveau.setHorizontalAlignment(JTextField.CENTER);
+
+		textarea.setBounds(0, 50, 650, 50);
 		textarea.setLineWrap(true);
 		textarea.setWrapStyleWord(true);
 		textarea.setBackground(new Color(255, 0, 0));
 		textarea.setForeground(new Color(255, 230, 51));
-		textarea.setFont(new Font("MV Boli",Font.BOLD,25));
+		textarea.setFont(new Font("MV Boli", Font.BOLD, 25));
 		textarea.setBorder(BorderFactory.createBevelBorder(1));
 		textarea.setEditable(false);
-		
-		buttonA.setBounds(0,100,100,100);
-		buttonA.setFont(new Font("MV Boli",Font.BOLD,35));
+
+		buttonA.setBounds(0, 100, 100, 100);
+		buttonA.setFont(new Font("MV Boli", Font.BOLD, 35));
 		buttonA.setFocusable(false);
 		buttonA.addActionListener(this);
 		buttonA.setText("A");
-		
-		buttonB.setBounds(0,200,100,100);
-		buttonB.setFont(new Font("MV Boli",Font.BOLD,35));
+
+		buttonB.setBounds(0, 200, 100, 100);
+		buttonB.setFont(new Font("MV Boli", Font.BOLD, 35));
 		buttonB.setFocusable(false);
 		buttonB.addActionListener(this);
 		buttonB.setText("B");
-		
-		buttonC.setBounds(0,300,100,100);
-		buttonC.setFont(new Font("MV Boli",Font.BOLD,35));
+
+		buttonC.setBounds(0, 300, 100, 100);
+		buttonC.setFont(new Font("MV Boli", Font.BOLD, 35));
 		buttonC.setFocusable(false);
 		buttonC.addActionListener(this);
 		buttonC.setText("C");
-		
-		answer_labelA.setBounds(125,100,500,100);
-		answer_labelA.setBackground(new Color(50,50,50));
-		answer_labelA.setForeground(new Color(25,255,0));
-		answer_labelA.setFont(new Font("MV Boli",Font.PLAIN,35));
-		
-		answer_labelB.setBounds(125,200,500,100);
-		answer_labelB.setBackground(new Color(50,50,50));
-		answer_labelB.setForeground(new Color(25,255,0));
-		answer_labelB.setFont(new Font("MV Boli",Font.PLAIN,35));
-		
-		answer_labelC.setBounds(125,300,500,100);
-		answer_labelC.setBackground(new Color(50,50,50));
-		answer_labelC.setForeground(new Color(25,255,0));
-		answer_labelC.setFont(new Font("MV Boli",Font.PLAIN,35));
-		
-		seconds_left.setBounds(535,510,100,100);
-		seconds_left.setBackground(new Color(25,25,25));
-		seconds_left.setForeground(new Color(255,0,0));
-		seconds_left.setFont(new Font("Ink Free",Font.BOLD,60));
+
+		answer_labelA.setBounds(125, 100, 500, 100);
+		answer_labelA.setBackground(new Color(50, 50, 50));
+		answer_labelA.setForeground(new Color(25, 255, 0));
+		answer_labelA.setFont(new Font("MV Boli", Font.PLAIN, 35));
+
+		answer_labelB.setBounds(125, 200, 500, 100);
+		answer_labelB.setBackground(new Color(50, 50, 50));
+		answer_labelB.setForeground(new Color(25, 255, 0));
+		answer_labelB.setFont(new Font("MV Boli", Font.PLAIN, 35));
+
+		answer_labelC.setBounds(125, 300, 500, 100);
+		answer_labelC.setBackground(new Color(50, 50, 50));
+		answer_labelC.setForeground(new Color(25, 255, 0));
+		answer_labelC.setFont(new Font("MV Boli", Font.PLAIN, 35));
+
+		seconds_left.setBounds(535, 510, 100, 100);
+		seconds_left.setBackground(new Color(25, 25, 25));
+		seconds_left.setForeground(new Color(255, 0, 0));
+		seconds_left.setFont(new Font("Ink Free", Font.BOLD, 60));
 		seconds_left.setBorder(BorderFactory.createBevelBorder(1));
 		seconds_left.setOpaque(true);
 		seconds_left.setHorizontalAlignment(JTextField.CENTER);
 		seconds_left.setText(String.valueOf(seconds));
-		
-		time_label.setBounds(535,475,100,25);
-		time_label.setBackground(new Color(50,50,50));
-		time_label.setForeground(new Color(255,0,0));
-		time_label.setFont(new Font("MV Boli",Font.PLAIN,16));
-		time_label.setHorizontalAlignment(JTextField.CENTER);
-		time_label.setText("timer >:D");
-		
-		number_right.setBounds(225,225,200,100);
-		number_right.setBackground(new Color(25,25,25));
-		number_right.setForeground(new Color(25,255,0));
-		number_right.setFont(new Font("Ink Free",Font.BOLD,50));
+//		
+//		time_label.setBounds(535,475,100,25);
+//		time_label.setBackground(new Color(50,50,50));
+//		time_label.setForeground(new Color(255,0,0));
+//		time_label.setFont(new Font("MV Boli",Font.PLAIN,16));
+//		time_label.setHorizontalAlignment(JTextField.CENTER);
+//		time_label.setText("timer >:D");
+
+		number_right.setBounds(225, 225, 200, 100);
+		number_right.setBackground(new Color(25, 25, 25));
+		number_right.setForeground(new Color(25, 255, 0));
+		number_right.setFont(new Font("Ink Free", Font.BOLD, 50));
 		number_right.setBorder(BorderFactory.createBevelBorder(1));
 		number_right.setHorizontalAlignment(JTextField.CENTER);
 		number_right.setEditable(false);
-		
-		percentage.setBounds(225,325,200,100);
-		percentage.setBackground(new Color(25,25,25));
-		percentage.setForeground(new Color(25,255,0));
-		percentage.setFont(new Font("Ink Free",Font.BOLD,50));
+
+		percentage.setBounds(225, 325, 200, 100);
+		percentage.setBackground(new Color(25, 25, 25));
+		percentage.setForeground(new Color(25, 255, 0));
+		percentage.setFont(new Font("Ink Free", Font.BOLD, 50));
 		percentage.setBorder(BorderFactory.createBevelBorder(1));
 		percentage.setHorizontalAlignment(JTextField.CENTER);
 		percentage.setEditable(false);
-		
+
 		frame.add(time_label);
 		frame.add(seconds_left);
 		frame.add(answer_labelA);
@@ -220,77 +184,93 @@ public class Quiz implements ActionListener{
 		frame.add(textarea);
 		frame.add(textfield);
 		frame.setVisible(true);
-		
+		frame.add(niveau);
+
 		nextQuestion();
 	}
+
 	public void nextQuestion() {
-		
-		if(index>=total_questions) {
+		if (index >= total_questions) {
 			results();
-		}
-		else {
-			textfield.setText("Question "+(index+1));
-			textarea.setText(questions[index]);
-			answer_labelA.setText(options[index][0]);
-			answer_labelB.setText(options[index][1]);
-			answer_labelC.setText(options[index][2]);
-			timer.start();
-		}
+		} else
+			for (int i = 0; i < questions.length; i++) {
+				// if(options[index].length!=2) {
+
+				textfield.setText("Question " + (nextqust + 1) + "/" + 15);
+				textarea.setText(questions[index]);
+				answer_labelA.setText(options[index][0]);
+				answer_labelB.setText(options[index][1]);
+				answer_labelC.setText(options[index][2]);
+				timer.start();
+				// }
+				if (index <= 5) {
+					niveau.setText("N1");
+				} else if (index <= 11) {
+					niveau.setText("N2");
+				} else if (index <= 17) {
+					niveau.setText("N3");
+
+//		}
+				}
+			}
 	}
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		
-			buttonA.setEnabled(false);
-			buttonB.setEnabled(false);
-			buttonC.setEnabled(false);
-			
-			if(e.getSource()==buttonA) {
-				answer= 'A';
-				if(answer == answers[index]) {
-					correct_guesses++;
-				}
-			}
-			if(e.getSource()==buttonB) {
-				answer= 'B';
-				if(answer == answers[index]) {
-					correct_guesses++;
-				}
-			}
-			if(e.getSource()==buttonC) {
-				answer= 'C';
-				if(answer == answers[index]) {
-					correct_guesses++;
-				}
-			}
 
-			displayAnswer();
-	}
-	public void displayAnswer() {
-		
-		timer.stop();
-		
 		buttonA.setEnabled(false);
 		buttonB.setEnabled(false);
 		buttonC.setEnabled(false);
-		
-		if(answers[index] != 'A')
-			answer_labelA.setForeground(new Color(255,0,0));
-		if(answers[index] != 'B')
-			answer_labelB.setForeground(new Color(255,0,0));
-		if(answers[index] != 'C')
-			answer_labelC.setForeground(new Color(255,0,0));
-		
+
+		if (e.getSource() == buttonA) {
+			answer = 'A';
+			if (answer == answers[index]) {
+				correct_guesses++;
+			}
+		}
+		if (e.getSource() == buttonB) {
+			answer = 'B';
+			if (answer == answers[index]) {
+				correct_guesses++;
+			}
+		}
+		if (e.getSource() == buttonC) {
+			answer = 'C';
+			if (answer == answers[index]) {
+				correct_guesses++;
+			}
+		}
+
+		displayAnswer();
+	}
+
+	public void displayAnswer() {
+
+		timer.stop();
+
+		buttonA.setEnabled(false);
+		buttonB.setEnabled(false);
+		buttonC.setEnabled(false);
+
+		if (answers[index] != 'A')
+			answer_labelA.setForeground(new Color(255, 0, 0));
+		if (answers[index] != 'B')
+			answer_labelB.setForeground(new Color(255, 0, 0));
+		if (answers[index] != 'C')
+			answer_labelC.setForeground(new Color(255, 0, 0));
+		nextqust++;
+
 		Timer pause = new Timer(2000, new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				answer_labelA.setForeground(new Color(25,255,0));
-				answer_labelB.setForeground(new Color(25,255,0));
-				answer_labelC.setForeground(new Color(25,255,0));
-				
+
+				answer_labelA.setForeground(new Color(25, 255, 0));
+				answer_labelB.setForeground(new Color(25, 255, 0));
+				answer_labelC.setForeground(new Color(25, 255, 0));
+
 				answer = ' ';
-				seconds=10;
+				seconds = 10;
 				seconds_left.setText(String.valueOf(seconds));
 				buttonA.setEnabled(true);
 				buttonB.setEnabled(true);
@@ -302,25 +282,29 @@ public class Quiz implements ActionListener{
 		pause.setRepeats(false);
 		pause.start();
 	}
-	public void results(){
-		
+
+	public void results() {
+
 		buttonA.setEnabled(false);
 		buttonB.setEnabled(false);
 		buttonC.setEnabled(false);
-		
-		result = (int)((correct_guesses/(double)total_questions)*100);
-		
+		label.setEnabled(false);
+		niveau.setEnabled(false);
+
+		result = (int) ((correct_guesses / (double) total_questions) * 100);
+
 		textfield.setText("RESULTS!");
 		textarea.setText("");
 		answer_labelA.setText("");
 		answer_labelB.setText("");
 		answer_labelC.setText("");
-		
-		number_right.setText("("+correct_guesses+"/"+total_questions+")");
-		percentage.setText(result+"%");
-		
+
+		number_right.setText("(" + correct_guesses + "/" + total_questions + ")");
+		percentage.setText(result + "%");
+
 		frame.add(number_right);
 		frame.add(percentage);
-		
+
 	}
 }
+
